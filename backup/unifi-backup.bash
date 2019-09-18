7985e15e0f658dc7c8cfba72bc9e1ae406a6655a
Konrad Komorowski <konrad@komorowski.me>
2019-09-18 15:37:03 +0100

    fix lint errors in the bash script (#7)

--- backup/unifi-backup.bash
+++ backup/unifi-backup.bash
@@ -11,7 +11,7 @@ REMOTE_BACKUP_DIR_DEFAULT="$REMOTE_BACKUP_DIR/default"
 
 rclone_sync () {
   echo "Syncing $1 to $2..."
-  rclone --config /var/rclone/rclone.conf sync $1 $2
+  rclone --config /var/rclone/rclone.conf sync "$1" "$2"
   echo "Done"
 }
 
@@ -20,24 +20,24 @@ mkdir -p $LOCAL_BACKUP_DIR
 
 while true
 do
-  if [[ "$(ls -A $LOCAL_BACKUP_DIR)" ]]; then
+  if [[ "$(find $LOCAL_BACKUP_DIR -type f)" ]]; then
     # Skip backup if files have not changed
-    if [[ ${BACKUP_HASH:-} == "$(ls -A $LOCAL_BACKUP_DIR | shasum)" ]]; then
+    if [[ ${BACKUP_HASH:-} == "$(find $LOCAL_BACKUP_DIR -type f | shasum)" ]]; then
       continue
     fi
 
     # Sync autobackup with the device specific cloud backup (this ensures that
     # every device has its last state available for inspection)
-    rclone_sync $LOCAL_BACKUP_DIR $REMOTE_BACKUP_DIR_DEVICE
+    rclone_sync "$LOCAL_BACKUP_DIR" "$REMOTE_BACKUP_DIR_DEVICE"
 
     # Sync the default cloud backup as well (this is used for auto restores)
-    rclone_sync $REMOTE_BACKUP_DIR_DEVICE $REMOTE_BACKUP_DIR_DEFAULT
+    rclone_sync "$REMOTE_BACKUP_DIR_DEVICE" "$REMOTE_BACKUP_DIR_DEFAULT"
 
     # Save the state
-    BACKUP_HASH=$(ls -A $LOCAL_BACKUP_DIR | shasum)
+    BACKUP_HASH=$(find $LOCAL_BACKUP_DIR -type f | shasum)
   else
     # Restore from default backup
-    rclone_sync $REMOTE_BACKUP_DIR_DEFAULT $LOCAL_BACKUP_DIR
+    rclone_sync "$REMOTE_BACKUP_DIR_DEFAULT" "$LOCAL_BACKUP_DIR"
   fi
 
   sleep 300
